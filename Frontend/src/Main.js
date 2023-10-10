da8b4c2f13901c17663724dd8e94afeac6c2e389
amir <jeomarkoo1@gmail.com>
2023-10-10 23:08:06 +0530

    adding more functionality

--- Frontend/src/Main.js
+++ Frontend/src/Main.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Route, Routes} from 'react-router-dom';
 import { Home } from './Components/Home/Home';
-import LeavesForm from './Components/LoginForm/LeavesForm';
+import LeavesForm from './Components/Forms/LeavesForm';
 
 export default function Main() {
     return (
